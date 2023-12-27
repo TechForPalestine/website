7ea7901161ee977663f30b2c739f226c383aa0ac
Rachouan Rejeb <info@rachouan.com>
2023-12-27 16:52:56 +0100

    Adds logo and fav

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,6 +3,19 @@ export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {
+      red: {
+        50: "#fef2f2",
+        100: "#fde3e3",
+        200: "#fccccc",
+        300: "#f9a8a8",
+        400: "#f37676",
+        500: "#e94a4a",
+        600: "#d83838",
+        700: "#b42121",
+        800: "#951f1f",
+        900: "#7c2020",
+        950: "#430c0c",
+      },
       green: {
         50: "#f0fdf5",
         100: "#dcfceb",
