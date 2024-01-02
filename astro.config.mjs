455ebd6b6d7e2425bff27409520a4e46f0d8d8bd
Rachouan Rejeb <info@rachouan.com>
2024-01-02 10:30:17 +0100

    Adds discord

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,14 @@
-import { defineConfig } from 'astro/config';
-
+import { defineConfig } from "astro/config";
+import icon from "astro-icon";
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind({
-    // Disable injecting a basic `base.css` import on every page.
-    applyBaseStyles: false,
-  })]
+  integrations: [
+    icon(),
+    tailwind({
+      // Disable injecting a basic `base.css` import on every page.
+      applyBaseStyles: false,
+    }),
+  ],
 });
