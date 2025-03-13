50c020ae984050ac84a2931e1538395d85fd7fb6
reehamRaed <reeham@leannode.com>
2025-03-13 16:44:14 +0200

    Tools page updates (#100)
    * remove token and update api file
    
    * fix volunteer form
    
    * fix the style add new tools  and change routes
    
    * update get-involved page (#94)
    
    Co-authored-by: Tom Hall <tom@Toms-MacBook-Pro.local>
    
    * Revert "Tools page updates (#95)" (#96)
    
    This reverts commit 24d2a698bd6ead3cc97015ca3416d24b91c3b45f.
    
    * fix the style add new tools  and change routes
    
    ---------
    
    Co-authored-by: Tom <31667058+tmhall99@users.noreply.github.com>
    Co-authored-by: Tom Hall <tom@Toms-MacBook-Pro.local>

--- src/components/ToolPopup.tsx
+++ src/components/ToolPopup.tsx
@@ -6,12 +6,13 @@ const ToolPopup = ({ buttonLabel, children }) => {
     return (
         <div>
             {/* Button to Open Popup */}
-            <button
+            <a
+                href="#"
                 onClick={() => setIsOpen(true)}
-                className="px-4 py-2 bg-blue-500 text-white rounded"
+                className="px-4 py-2 bg-green-800 text-white rounded hover:cursor-pointer"
             >
                 {buttonLabel}
-            </button>
+            </a>
 
             {/* Popup Overlay */}
             {isOpen && (
