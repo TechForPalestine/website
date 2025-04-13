c901c70c3d4188103b9fbd31c81d9f6d8133ee35
reehamRaed <reeham@leannode.com>
2025-04-13 17:44:16 +0200

    implement rescues page (#123)

---
+++ src/components/FAQAccordion.tsx
@@ -0,0 +1,31 @@
+import * as React from 'react';
+import {
+    Accordion,
+    AccordionSummary,
+    AccordionDetails,
+    Typography,
+    Link
+} from '@mui/material';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+
+type FAQAccordionProps = {
+    question: string;
+    answer: React.ReactNode;
+};
+
+export default function FAQAccordion({ question, answer }: FAQAccordionProps) {
+    return (
+        <Accordion>
+            <AccordionSummary
+                expandIcon={<ExpandMoreIcon />}
+                aria-controls="faq-content"
+                id={`faq-${question}`}
+            >
+                <Typography>{question}</Typography>
+            </AccordionSummary>
+            <AccordionDetails>
+                <Typography>{answer}</Typography>
+            </AccordionDetails>
+        </Accordion>
+    );
+}
