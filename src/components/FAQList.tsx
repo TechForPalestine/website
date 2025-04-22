80a698e32f228fbe7a36eaad4db44a923411be02
reehamRaed <reeham@leannode.com>
2025-04-22 22:39:16 +0200

    108 update hire palestinians page (#132)
    * implement rescues page
    
    * Load Donate resource only after clicking the Donate button
    
    * Update "Hire Palestinians" page

--- src/components/FAQList.tsx
+++ src/components/FAQList.tsx
@@ -1,6 +1,91 @@
 import * as React from 'react';
 import FAQAccordion from './FAQAccordion';
 import { Link, Typography } from '@mui/material';
+import ResourceList from './ResourceList';
+
+const resourceSections = [
+    {
+        name: 'Recruitment Organizations',
+        resources: [
+            {
+                name: 'Apricot',
+                url: 'https://apricotinternational.org/',
+                logo: '/apricot.jpg',
+                description: `Experienced MENA Engineers delivering results and contributing to your product's growth immediately.`,
+            },
+            {
+                name: 'MENA Alliances',
+                url: 'https://menaalliances.com/jobsforpalestine/',
+                logo: '/mena-alliances-logo.webp',
+                description: `Empowering Palestinian Professionals With Job Opportunities`,
+            },
+            {
+                name: 'Gaza Sky Geeks',
+                url: 'https://gazaskygeeks.com/',
+                logo: '/gaza-sky-geeks-logo.png',
+                description: `A leading tech hub in Gaza and the West Bank, fostering the digital economy and future technology leaders since 2011.`,
+            },
+            {
+                name: 'Manara.tech',
+                url: 'https://manara.tech/tech-jobs-for-palestine',
+                logo: '/manara.png',
+                description: `Built to connect top tech talent in the MENA region to global jobs.`,
+            },
+            {
+                name: 'Rising from Ashes',
+                url: 'https://bees.to/hire-ps/',
+                logo: '/bees-logo-horizontal.webp',
+                description: `Connects global companies with remote workers in Palestine via partners like Bees, Flow Accelerator, and GSG.`,
+            },
+            {
+                name: 'TAP',
+                url: 'https://www.tapcareers.io/',
+                logo: '/tap-logo.svg',
+                description: `A career accelerator in Palestine and Jordan offering remote-ready talent.`,
+            },
+            {
+                name: 'Techtative',
+                url: 'https://techtative.io/',
+                logo: '/techtative-logo.png',
+                description: `Premier EOR in Palestine, enabling global orgs to tap into local expertise.`,
+            },
+            {
+                name: 'Olives and Heather',
+                url: 'https://www.olivesandheather.com/',
+                logo: '/olives-and-heather.png',
+                description: `Empowers a new generation of Palestinian marketers through global marketing excellence.`,
+            },
+            {
+                name: 'Gaza Talents',
+                url: 'https://gazatalents.com/en',
+                logo: '/gazatalents.png',
+                description: `Provide quick access for those seeking Professional Skills from Gaza.`,
+            },{
+                name: 'Youmna',
+                url: 'https://www.withyoumna.com/',
+                logo: '/youmna.png',
+                description: `Achieve more with a proactive, remote Executive Assistant who acts as your "right hand".`,
+            },
+        ]
+    },
+    {
+        name: 'Freelance',
+        resources: [
+            {
+                name: 'Dribble',
+                url: 'https://dribbble.com/freelance-ux-designers-for-hire-gaza-ps',
+                logo: '/dribbble-logo.png',
+                description: `UX designers from Gaza offering freelance services.`,
+            },
+            {
+                name: 'Upwork',
+                url: 'https://www.upwork.com/hire/ps/',
+                logo: '/upwork.png', // SVG string if desired
+                description: `Hire freelancers from Palestine through Upwork.`,
+            },
+        ]
+    }
+];
 
 const faqs = [
     {
@@ -56,8 +141,18 @@ const faqs = [
             </>
         )
     },
+    {
+        question: 'How do I hire Palestinians?',
+        answer: (
+            <>
 
+                <ResourceList  sections={resourceSections}  />
+
+            </>
+        )
+    }
 ];
+
 
 export default function FAQList() {
     return (
