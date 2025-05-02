d0c90744a4a15712891cfe221271f47c0c3a4745
reehamRaed <reeham@leannode.com>
2025-05-02 15:10:44 +0300

    126 create site index in footer (#135)
    * implement rescues page
    
    * Load Donate resource only after clicking the Donate button
    
    * Create site index in footer

---
+++ src/components/navigation.ts
@@ -0,0 +1,13 @@
+// src/components/navigation.ts
+export const navigation = new Map<string, string>([
+    ["About", "/about"],
+    ["Incubator", "/incubator"],
+    ["Projects", "/project"],
+    ["Blog", "https://updates.techforpalestine.org"],
+    ["Hire Palestinians", "/help/hire"],
+    ["Media", "mailto:media@techforpalestine.org"],
+    ["Get Involved", "/get-involved"],
+    ["Donate", "/donate"],
+    ["Tools", "/tools"],
+    ["Resources", "/resources"],
+]);
