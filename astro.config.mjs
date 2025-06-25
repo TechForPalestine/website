a41156e7447eccde77a19fa28b750d5a5598410b
reehamRaed <reeham93@gmail.com>
2025-06-25 12:59:16 +0300

    Events page (#171)
    * create events page
    
    * add event and event details pages
    
    * sort by date
    
    * remove event details page

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'server',
+  // output: 'server',
   integrations: [
     icon(),
     react(),
