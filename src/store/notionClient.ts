a50c67f038b2624ca98a2f1b2371a65c0228642c
ReehamRaed <reeham93@gmail.com>
2025-07-03 13:30:07 +0300

    read projects from notion

--- src/store/notionClient.ts
+++ src/store/notionClient.ts
@@ -3,6 +3,7 @@ import axios from 'axios';
 
 const NOTION_SECRET = import.meta.env.NOTION_SECRET;
 const NOTION_DB_ID = import.meta.env.NOTION_DB_ID;
+const NOTION_PROJECTS_DB_ID = import.meta.env.NOTION_PROJECTS_DB_ID;
 
 const notionAxios = axios.create({
     baseURL: "https://api.notion.com/v1/",
@@ -15,6 +16,7 @@ const notionAxios = axios.create({
 export const fetchNotionEvents = async () => {
     const response = await notionAxios.post(`databases/${NOTION_DB_ID}/query`);
 
+    if (!NOTION_DB_ID) console.warn("NOTION_DB_ID is missing");
     const events = response.data.results.map((page: any) => {
         const props = page.properties;
 
@@ -33,7 +35,6 @@ export const fetchNotionEvents = async () => {
 
         const registerLink = props["Link to registration"]?.url || "";
         const recordingLink = props["Link to recording"]?.url || "";
-
         return {
             id: page.id,
             title: props["Title"]?.title?.[0]?.plain_text || "Untitled",
@@ -51,7 +52,36 @@ export const fetchNotionEvents = async () => {
     // Sort by date (ascending)
     return events.sort((a, b) => new Date(b.date).getTime() - new Date(a.date).getTime());
 };
+export const fetchProjectsFromNotion = async () => {
+    if (!NOTION_DB_ID) {
+        throw new Error("Missing Notion credentials");
+    }
 
+    const res  = await notionAxios.post(
+        `databases/${NOTION_PROJECTS_DB_ID}/query`,
+
+    );
+    const results = res.data.results;
+
+    return results.map((page: any) => {
+        const props = page.properties;
+
+        const title = props["Project Name"]?.rich_text?.[0]?.plain_text ?? "Untitled Project";
+
+        const description = props["Project Description"]?.rich_text?.[0]?.plain_text ?? "";
+
+        const website = props["Website URL"]?.url ?? "";
+
+        const discord_channel = props["Co-Founders Discord Username(s)"]?.rich_text?.[0]?.plain_text ?? "";
+
+        return {
+            project_name: title,
+            elevator_pitch: description,
+            website,
+            discord_channel,
+        };
+    });
+};
 export const fetchNotionEventById = async (pageId: string) => {
 
     const response = await notionAxios.get(`pages/${pageId}`);
