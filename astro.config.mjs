2d507ee846715ee40232c985746bfa4fb3e7ac0f
mohanadft <mohanadfteha@gmail.com>
2025-09-17 12:29:30 +0300

    fix: use hybrid with prerender capabilities

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'server',
+  output: 'hybrid',
   adapter: cloudflare(),
   integrations: [
     icon(),
--- src/pages/api/faq.ts
+++ src/pages/api/faq.ts
@@ -1,6 +1,8 @@
 import type { APIRoute } from 'astro';
 import { fetchNotionFAQ } from '../../store/notionClient';
 
+export const prerender = false;
+
 export const GET: APIRoute = async ({request}) => {
     try {
         const url = new URL(request.url);
