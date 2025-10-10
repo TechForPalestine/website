1d7c76a04f2efbe5f685e507596223aeac7b8f8c
abdo.spices <spices.digital@gmail.com>
2025-10-10 23:57:40 +0300

    refactor: update project deps & use pnpm

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'hybrid',
+  output: 'server',
   adapter: cloudflare({
     imageService: 'cloudflare'
   }),
